6f764b70956d193271844606ffaab5bff28f6129
Jaz-ma <potatoorgy3@gmail.com>
2022-05-22 21:46:03 +0100

    fetch api

--- app.js
+++ app.js
@@ -4,11 +4,28 @@ const app = Vue.createApp({
             firstName:'Yacine',
             lastName:'Benaouda',
             email:"yacine@gmail.com",
-            gender:'Male',
+            gender:'male',
             portrait:'https://randomuser.me/portraits/men/10.jpg'
 
 
         }
+    },
+    methods:{
+
+    async getUser(){
+        const res = await fetch('https://randomuser.me/api')
+        const {results} = await res.json()
+
+            this.firstName ='Sarah',
+            this.lastName ='Ghadi',
+            this.email="sarah@gmail.com",
+            this.gender='female',
+            this.portrait='https://randomuser.me/portraits/women/10.jpg'
+
+
+        console.log(results);
+    }
+
     }
 });
 
